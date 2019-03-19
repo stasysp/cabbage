3e3c610c57377114d8214b14cc24c1fa02b9eee8
Anastasiya Popova <anastasiya.popova@xperience.ai>
2019-03-19 21:53:15 +0300

    Some stupid fixes

--- install.sh
+++ install.sh
@@ -1,7 +1,14 @@
+<<<<<<< HEAD
+pip3 install git+https://github.com/justayak/cabbage.git
+pip3 install git+https://github.com/justayak/pak.git
+pip3 install git+https://github.com/justayak/cselect.git
+pip3 install git+https://github.com/justayak/pppr.git
+=======
 pip install git+https://github.com/justayak/cabbage.git
 pip install git+https://github.com/justayak/pak.git
 pip install git+https://github.com/justayak/cselect.git
 pip install git+https://github.com/justayak/pppr.git
+>>>>>>> d7c73556e97d2b11770bfe8513d6153223375c44
 
 mkdir build
 (cd build && cmake .. && make -j8)
@@ -11,5 +18,8 @@ if [ ! -d "deepmatching_bin" ]; then
     (cd deepmatching_bin && wget http://lear.inrialpes.fr/src/deepmatching/code/deepmatching_1.2.2.zip && unzip deepmatching_1.2.2.zip)
 fi
 
+<<<<<<< HEAD
+=======
 
+>>>>>>> d7c73556e97d2b11770bfe8513d6153223375c44
 python build_settings.py
